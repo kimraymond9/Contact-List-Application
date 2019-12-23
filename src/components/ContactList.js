b656b3807554ad98f2850d502d8e6fa88bd1aca4
Raymond Kim <kimraymond9@gmail.com>
2019-12-24 00:07:45 +1300

    Showing the users in a list

---
+++ src/components/ContactList.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import '../App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+
+
+function ContactList(props) {
+    const useStyles = makeStyles(theme => ({
+        root: {
+            width: '100%',
+            maxWidth: 360,
+            backgroundColor: theme.palette.background.paper,
+        },
+        inline: {
+            display: 'inline',
+        },
+    }));
+
+    const classes = useStyles();
+    
+    if (!props.data){
+        return null;
+    }else{
+        return (
+            <div>
+                <List className={classes.root}>
+                        {props.data.map(user => {
+                            return (
+                                <div>
+                                <ListItem alignItems="flex-start">
+                                    <ListItemAvatar>
+                                            <Avatar alt="Remy Sharp" src="https://i.pravatar.cc/300" />
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary={user.name}
+                                        secondary={
+                                            <React.Fragment>
+                                                <Typography
+                                                    component="span"
+                                                    variant="body2"
+                                                    className={classes.inline}
+                                                    color="textPrimary"
+                                                >
+                                                    {user.username}
+                                    </Typography>
+                                                {" — " + user.email}
+                                            </React.Fragment>
+                                        }
+                                    />
+                                </ListItem>
+                                <Divider variant="inset" component="li" />
+                                </div>
+                            )
+                        }
+                    )}
+                </List>
+            </div>
+        )
+    }
+}
+
+
+export default ContactList;
---
+++ src/components/FetchJSON.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../App.css';
+import ContactList from './ContactList';
+
+class FetchJSON extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null,
+        };
+    }
+
+    componentDidMount() {
+        return fetch('http://jsonplaceholder.typicode.com/users')
+                .then((response) => response.json())
+                .then((responseJson) => {
+                    this.setState({data: responseJson})
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    
+
+    
+    render() {
+        return (
+            <div>
+                <ContactList data = {this.state.data}/>
+            </div>
+        )
+    }
+}
+
+export default FetchJSON;
