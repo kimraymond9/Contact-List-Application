b5b50e516d7449b04698fffc064587cd4333bc5a
Raymond Kim <kimraymond9@gmail.com>
2020-01-04 02:54:37 +1300

    passed appriopriate props to test components

--- src/Contacts/ContactDialog.test.js
+++ src/Contacts/ContactDialog.test.js
@@ -4,6 +4,28 @@ import ContactDialog from './ContactDialog';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  render(<ContactDialog />, div);
+  render(<ContactDialog user={{
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    address: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough',
+      zipcode: '92998-3874',
+      geo: {
+        lat: '-37.3159',
+        lng: '81.1496'
+      }
+    },
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org',
+    company: {
+      name: 'Romaguera-Crona',
+      catchPhrase: 'Multi-layered client-server neural-net',
+      bs: 'harness real-time e-markets'
+    }
+  }}/>, div);
   unmountComponentAtNode(div);
 });
