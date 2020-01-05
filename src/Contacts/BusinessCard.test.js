d3da57a521b8c20146ec1066a72215ce39e95d5c
Raymond Kim <kimraymond9@gmail.com>
2020-01-05 19:02:37 +1300

    Updated tests for Business Card

--- src/Contacts/BusinessCard.test.js
+++ src/Contacts/BusinessCard.test.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import BusinessCard from "./BusinessCard";
 import contacts from "../contacts.json";
 
-it("displays the correct user information", () => {
-  const { getByText } = render(<BusinessCard user={contacts[0]} />);
-  expect(getByText("Leanne Graham")).toBeInTheDocument();
-  expect(getByText("Romaguera-Crona")).toBeInTheDocument();
-  expect(getByText("1-770-736-8031 x56442")).toBeInTheDocument();
-  expect(getByText("Sincere@april.biz")).toBeInTheDocument();
-  expect(getByText("hildegard.org")).toBeInTheDocument();
+it("progress indicator is displayed while business card is being fetched", () => {
+  const { getByTestId } = render(<BusinessCard user={contacts[0]} />);
+  // a progress indicator should be displayed while the catchphrase is being fetched.
+  expect(getByTestId("business-card-progress-indicator")).toBeInTheDocument();
+});
+
+it("business card is displayed after the image is loaded", async () => {
+  const { getByTestId, queryByTestId } = render(
+    <BusinessCard user={contacts[0]} />
+  );
+  // the company catchphrase should be displayed once the data is fetched.
+  expect(queryByTestId("business-card")).not.toBeInTheDocument();
+  fireEvent.load(getByTestId("business-card-image"));
+  expect(getByTestId("business-card")).toBeInTheDocument();
 });
