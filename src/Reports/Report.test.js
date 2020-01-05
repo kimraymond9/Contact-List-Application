695f4a27c5812496a6cd29044a1068f6e606b015
Raymond Kim <kimraymond9@gmail.com>
2020-01-05 17:41:11 +1300

    Wrote tests for reports

--- src/Reports/Report.test.js
+++ src/Reports/Report.test.js
@@ -1,38 +1,15 @@
 import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
+import { render } from "@testing-library/react";
 import Report from "./Report";
+import contacts from "../contacts.json";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  render(
-    <Report
-      data={[
-        {
-          id: 1,
-          name: "Leanne Graham",
-          username: "Bret",
-          email: "Sincere@april.biz",
-          address: {
-            street: "Kulas Light",
-            suite: "Apt. 556",
-            city: "Gwenborough",
-            zipcode: "92998-3874",
-            geo: {
-              lat: "-37.3159",
-              lng: "81.1496"
-            }
-          },
-          phone: "1-770-736-8031 x56442",
-          website: "hildegard.org",
-          company: {
-            name: "Romaguera-Crona",
-            catchPhrase: "Multi-layered client-server neural-net",
-            bs: "harness real-time e-markets"
-          }
-        }
-      ]}
-    />,
-    div
-  );
-  unmountComponentAtNode(div);
+jest.mock("react-chartjs-2", () => ({
+  Bar: () => null
+}));
+
+it("displays the correct user information", () => {
+  const { getByTestId } = render(<Report data={contacts} />);
+  expect(getByTestId("bar-graph")).toBeInTheDocument();
+  expect(getByTestId("interactive-map")).toBeInTheDocument();
+  expect(getByTestId("bar-graph-domain")).toBeInTheDocument();
 });
