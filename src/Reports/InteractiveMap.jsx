249c45bb6947425a9c273770e32d8432816ef9d7
Raymond Kim <kimraymond9@gmail.com>
2020-01-05 17:40:51 +1300

    Wrote tests for interactive map

--- src/Reports/InteractiveMap.jsx
+++ src/Reports/InteractiveMap.jsx
@@ -5,26 +5,28 @@ import "../leaflet.css";
 
 const InteractiveMap = props => {
   return (
-    <Map center={[-40, -40]} zoom={1}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      {props.data.map(user => {
-        return (
-          <Marker
-            key={user.id}
-            position={[user.address.geo.lat, user.address.geo.lng]}
-          >
-            <Popup>
-              {user.name}
-              <br />
-              {user.company.name}
-            </Popup>
-          </Marker>
-        );
-      })}
-    </Map>
+    <div data-testid="interactive-map">
+      <Map center={[-40, -40]} zoom={1}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {props.data.map(user => {
+          return (
+            <Marker
+              key={user.id}
+              position={[user.address.geo.lat, user.address.geo.lng]}
+            >
+              <Popup>
+                {user.name}
+                <br />
+                {user.company.name}
+              </Popup>
+            </Marker>
+          );
+        })}
+      </Map>
+    </div>
   );
 };
 
--- src/Reports/InteractiveMap.test.js
+++ src/Reports/InteractiveMap.test.js
@@ -1,38 +1,9 @@
 import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
+import { render } from "@testing-library/react";
 import InteractiveMap from "./InteractiveMap";
+import contacts from "../contacts.json";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  render(
-    <InteractiveMap
-      data={[
-        {
-          id: 1,
-          name: "Leanne Graham",
-          username: "Bret",
-          email: "Sincere@april.biz",
-          address: {
-            street: "Kulas Light",
-            suite: "Apt. 556",
-            city: "Gwenborough",
-            zipcode: "92998-3874",
-            geo: {
-              lat: "-37.3159",
-              lng: "81.1496"
-            }
-          },
-          phone: "1-770-736-8031 x56442",
-          website: "hildegard.org",
-          company: {
-            name: "Romaguera-Crona",
-            catchPhrase: "Multi-layered client-server neural-net",
-            bs: "harness real-time e-markets"
-          }
-        }
-      ]}
-    />,
-    div
-  );
-  unmountComponentAtNode(div);
+it("displays the map", () => {
+  const { getByTestId } = render(<InteractiveMap data={contacts} />);
+  expect(getByTestId("interactive-map")).toBeInTheDocument();
 });
